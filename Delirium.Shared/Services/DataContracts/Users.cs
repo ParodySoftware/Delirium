aef776232183e0808a857a7173ead242dc391065
Ninjaquartz <radoslav.mihaylov@gmail.com>
2014-10-30 15:58:33 +0100

    #2 Update missing file

---
+++ Delirium.Shared/Services/DataContracts/Users.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delirium.Shared.Services.DataContracts
+{
+    public class User
+    {
+        public string Id { get; set; }
+
+        public object Created { get; set; }
+
+        public object Deleted { get; set; }
+
+        public IEnumerable<object> Postings { get; set; }
+
+        public string Name { get; set; }
+
+        public string Mail { get; set; }
+
+        public object Phone { get; set; }
+
+        public bool Enabled { get; set; }
+
+        public IEnumerable<Role> Roles { get; set; }
+
+        public string Password { get; set; }
+    }
+}
